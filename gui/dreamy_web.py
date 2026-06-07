46c104fa66b0f7de5c70387cd39368251cc91bff
zsomagus <zsomagus@gmail.com>
2026-06-07 10:39:29 +0200

    requests import pótlása

--- gui/dreamy_web.py
+++ gui/dreamy_web.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import requests
 # Megkeressük a gui mappa szülőmappáját (a projekt gyökerét)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if BASE_DIR not in sys.path:
